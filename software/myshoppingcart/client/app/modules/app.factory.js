b87a1b388597ab15c66852ded66bcfb6372db363
agaffar <abdul.gaffar@india.semanticbits.com>
2017-01-24 20:24:24 +0530

    directives

---
+++ software/myshoppingcart/client/app/modules/app.factory.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Lenovo on 1/24/2017.
+ */
+(function () {
+'use strict'
+    angular.module('myApp').factory('appFactory',appFactory);
+    appFactory.$inject = ['$http','$q'];
+    function appFactory($http,$q)
+    {
+        var factoryServices = { };
+        factoryServices.getJData = getDetails;
+        return factoryServices;
+
+        function getDetails()
+        {
+            //console.log("in details");
+            /* return $http.get('dataset.json');*/
+
+            // simulated async function
+            var defered = $q.defer();
+            $http.get('dataset.json').success(function (response)
+            {
+                //defered = response.data;
+                defered.resolve(response);
+                console.log(defered.promise);
+            }).error(function (response){
+                defered.reject("failed to load json");
+            })
+
+
+            return defered.promise;
+        }
+
+    }
+})();
---
+++ software/myshoppingcart/client/app/modules/app.run.js
@@ -0,0 +1,26 @@
+(function()
+{
+    'use strict'
+    angular.module("myApp").run(myAppRun);
+
+    myAppRun.$inject = ['appFactory','$rootScope'];
+    function myAppRun(appFactory,$rootScope)
+    {
+        console.log("in my app run");
+        appFactory.getJData().then(function(response)
+        {
+            console.log("in app run");
+            console.log(response);
+            $rootScope.products = [];
+            $rootScope.products = response;
+            //vm.products =
+            //console.log(vm.products);
+        },function(data)
+        {
+            //console.log(response);
+
+            //console.log(vm.products);
+        });
+    }
+
+})();
